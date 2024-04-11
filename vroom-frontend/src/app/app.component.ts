4645c9499052ee0a64260abd7156fdab9aa23efb
ufuksaylan <ufuksaylannn@gmail.com>
2024-04-12 01:39:15 +0300

    create new component for leasing info part and reset css and provide animation in appconfig

--- vroom-frontend/src/app/app.component.ts
+++ vroom-frontend/src/app/app.component.ts
@@ -3,13 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { environment } from '../environment/environment';
 import { WizardComponent } from './components/wizard/wizard.component';
 
-
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, WizardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'vroom-frontend';
