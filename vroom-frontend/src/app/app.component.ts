d3a7f4711db9052a6ecc444b128610b7c3370b4c
ufuksaylan <66475538+ufuksaylan@users.noreply.github.com>
2024-04-11 16:59:33 +0300

    Experimental/deployment (#1)
    * add deployment configuration
    
    * add configuration file

--- vroom-frontend/src/app/app.component.ts
+++ vroom-frontend/src/app/app.component.ts
@@ -1,13 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environment/environment';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'vroom-frontend';
+  apiHost = environment.apiHost;
 }
