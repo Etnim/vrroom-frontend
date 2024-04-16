a4442d5df5c69507e62ddc94e9dfb28d7ca6aac0
[Crazyem] <93625165+Xtrazyem@users.noreply.github.com>
2024-04-16 17:56:16 +0300

    merging dev

--- vroom-frontend/src/app/components/wizard/types.ts
+++ vroom-frontend/src/app/components/wizard/types.ts
@@ -38,16 +38,17 @@ export interface PersonalAndContactInfoFormGroup {
   address: FormControl<string | null>;
   city: FormControl<string | null>;
   postalCode: FormControl<string | null>;
-
 }
 
+<<<<<<< HEAD
+=======
 export interface ReviewAndSubmitFormGroup {
   reviewConfirm: FormControl<boolean | null>;
 }
 export interface CompleteFormData {
   leasingInfo: LeasingInfoFormGroup;
   financialInfo: FinancialInfoFormGroup;
-  vehicleInfo: VehicleInfoFormGroup;
   personalInfo: PersonalAndContactInfoFormGroup;
-  reviewConfirm: ReviewAndSubmitFormGroup;
+  // You can add additional fields if there are other sections/forms
 }
+>>>>>>> 2f2e938 (add review component)
