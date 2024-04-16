3cbd614dde70e7cf2547f2de0155c53eb86f7e4f
[Crazyem] <93625165+Xtrazyem@users.noreply.github.com>
2024-04-16 20:16:49 +0300

    Added same scss to all components. made a somewhat working skeleton for review

--- vroom-frontend/src/app/components/wizard/wizard.component.ts
+++ vroom-frontend/src/app/components/wizard/wizard.component.ts
@@ -17,21 +17,21 @@ import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { AsyncPipe } from '@angular/common';
 import { LeasingInfoComponentComponent } from './leasing-info-component/leasing-info-component.component';
-<<<<<<< HEAD
-import { type FinancialInfoFormGroup, type LeasingInfoFormGroup, type VehicleInfoFormGroup, type PersonalAndContactInfoFormGroup } from './types';
-=======
-import { type ReviewAndSubmitFormGroup, type FinancialInfoFormGroup, type LeasingInfoFormGroup, type PersonalAndContactInfoFormGroup } from './types';
->>>>>>> 2f2e938 (add review component)
+
+import { 
+  type FinancialInfoFormGroup, 
+  type LeasingInfoFormGroup, 
+  type VehicleInfoFormGroup, 
+  type PersonalAndContactInfoFormGroup, 
+  type ReviewAndSubmitFormGroup, 
+  type CompleteFormData } from './types';
+
 import { FinancialInfoComponent } from './financial-info/financial-info.component';
 import { VehicleInfoComponent } from './vehicle-info/vehicle-info.component';
 import { HttpClientModule } from '@angular/common/http';
 import { PersonalContactInfoComponent } from './personal-contact-info/personal-contact-info.component';
-<<<<<<< HEAD
-
-
-=======
 import { ReviewAndSubmitComponent } from './review-and-submit/review-and-submit.component';
->>>>>>> 2f2e938 (add review component)
+
 /**
  * @title Stepper responsive
  */
@@ -84,7 +84,7 @@ export class WizardComponent {
     monthlyObligations: new FormControl<number | null>(null, [Validators.required, Validators.min(1), Validators.max(100000)])
   });
 
-  thirdFormGroup = this._formBuilder.group({
+  thirdFormGroup = this._formBuilder.group<VehicleInfoFormGroup>({
     make: new FormControl<string | null>(null, Validators.required),
     model: new FormControl<string | null>(null, Validators.required),
     year: new FormControl<number | null>(null, [Validators.required, Validators.min(2010), Validators.max(2024)]),
@@ -106,6 +106,7 @@ export class WizardComponent {
   fifthFormGroup = this._formBuilder.group<ReviewAndSubmitFormGroup>({
     reviewConfirm: new FormControl<boolean | null>(null, Validators.required),
   });
+  
   stepperOrientation: Observable<StepperOrientation>;
 
   constructor(private _formBuilder: FormBuilder, breakpointObserver: BreakpointObserver) {
