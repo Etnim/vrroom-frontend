b054d1544b023c39789e623766c06020d3bdb382
Oskaras-coder <osafinas@gmail.com>
2024-04-17 15:52:09 +0300

    merge dev

--- vroom-frontend/src/app/app.routes.ts
+++ vroom-frontend/src/app/app.routes.ts
@@ -1,5 +1,8 @@
 import { Routes } from '@angular/router';
 import { WizardComponent } from './components/wizard/wizard.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
+
 export const routes: Routes = [
-  { path: '', component: WizardComponent }
+  { path: '', component: WizardComponent},
+  { path: 'admin', component: AdminDashboardComponent},
 ];
