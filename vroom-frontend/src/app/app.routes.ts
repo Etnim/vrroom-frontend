fc3063f7b82e5f71a373c3a7219829d9404b81bd
Oskaras-coder <osafinas@gmail.com>
2024-04-17 15:49:49 +0300

    Add admin dashboard page

--- vroom-frontend/src/app/app.routes.ts
+++ vroom-frontend/src/app/app.routes.ts
@@ -1,4 +1,9 @@
 import { Routes } from '@angular/router';
 import { WizardComponent } from './components/wizard/wizard.component';
+import { AdminDashboardComponent } from './components/admin-dashboard/admin-dashboard.component';
 
-export const routes: Routes = [{ path: '', component: WizardComponent }];
+export const routes: Routes = [
+  { path: '', component: WizardComponent},
+  { path: 'admin', component: AdminDashboardComponent},
+  
+];
