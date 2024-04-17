71c2e4f7369251fddf50ef0f542b55e2a35a4718
Martynas Piliutis <martynas.piliutis@gmail.com>
2024-04-17 13:11:18 +0300

    removed unnecessary route

--- vroom-frontend/src/app/app.routes.ts
+++ vroom-frontend/src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
 import { WizardComponent } from './components/wizard/wizard.component';
-import {CalculatorComponent} from "./components/calculator/calculator.component";
 export const routes: Routes = [
-  { path: '', component: WizardComponent },
-  { path: 'calculator', component: CalculatorComponent}
+  { path: '', component: WizardComponent }
 ];
