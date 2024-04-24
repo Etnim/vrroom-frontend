e2a9257a5d78a190b49e2c6af3e81be313c15797
ufuksaylan <66475538+ufuksaylan@users.noreply.github.com>
2024-04-24 22:39:15 +0300

    add post request (#25)

--- vroom-frontend/src/app/types/vehicle-details.ts
+++ vroom-frontend/src/app/types/vehicle-details.ts
@@ -1,10 +1,10 @@
 import { z } from 'zod';
 
-export enum FuelType {
-  PETROL,
-  DIESEL,
-  ELECTRIC,
-  HYBRID
+enum FuelType {
+  PETROL = 'PETROL',
+  DIESEL = 'DIESEL',
+  ELECTRIC = 'ELECTRIC',
+  HYBRID = 'HYBRID'
 }
 
 export const vehicleDetailsSchema = z.object({
@@ -12,11 +12,40 @@ export const vehicleDetailsSchema = z.object({
   brand: z.string(),
   model: z.string(),
   year: z.number().int(),
-  fuel: z.nativeEnum(FuelType),
-  emission: z.number().int()
+  fuel: z.string().refine((val) => Object.values(FuelType).includes(val as FuelType), {
+    message: 'Invalid marital status'
+  }),
+  emissionStart: z.number().int(),
+  emissionEnd: z.number().int()
 });
 
 export type VehicleDetails = z.infer<typeof vehicleDetailsSchema>;
 
 export const vehicleDetailsInsertSchema = vehicleDetailsSchema.omit({ id: true });
 export type VehicleDetailsInsert = z.infer<typeof vehicleDetailsInsertSchema>;
+
+export function mapFormValueToVehicleDetailsInsert(
+  formValue: {
+    brand: string | null;
+    model: string | null;
+    year: number | null;
+    fuel: string | null;
+  },
+  emissionForm: {
+    emissionStart: number | null;
+    emissionEnd: number | null;
+  }
+): VehicleDetailsInsert {
+  if (formValue.fuel === null) {
+    throw new Error('Invalid fuel type');
+  }
+
+  return {
+    brand: formValue.brand!,
+    model: formValue.model!,
+    year: Math.floor(formValue.year!),
+    fuel: formValue.fuel.toUpperCase(),
+    emissionStart: emissionForm.emissionStart!,
+    emissionEnd: emissionForm.emissionEnd!
+  };
+}
