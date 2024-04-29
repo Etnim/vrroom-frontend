3b514ff22b19b4abb18065975e934c20672b9ae7
Oskaras-coder <osafinas@gmail.com>
2024-04-30 00:16:56 +0300

    apiHost changed to render

--- vroom-frontend/src/app/services/application.service.ts
+++ vroom-frontend/src/app/services/application.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../../environment/environment';
 import type { CustomerData } from '../types/requests';
