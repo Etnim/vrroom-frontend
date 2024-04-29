195dc7d55daf730631b3bd4c3deda1df3190ff69
ufuksaylan <66475538+ufuksaylan@users.noreply.github.com>
2024-04-29 12:54:26 +0300

    add initial config for firebase and login-register (#31)
    * add initial config for firebase and login-register
    
    * add more budget :p
    
    * add jwt interceptor
    
    * remove google imports,realized I dont use them

---
+++ vroom-frontend/src/app/services/auth.service.ts
@@ -0,0 +1,60 @@
+import { Injectable, inject, signal } from '@angular/core';
+import {
+  Auth,
+  createUserWithEmailAndPassword,
+  updateProfile,
+  signInWithEmailAndPassword,
+  user,
+  signOut,
+  signInWithPopup,
+  GoogleAuthProvider
+} from '@angular/fire/auth';
+import { Observable, from } from 'rxjs';
+import { UserInterface } from '../types/user-interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  fireBaseAuth = inject(Auth);
+  user$ = user(this.fireBaseAuth);
+  currentUserSig = signal<UserInterface | null | undefined>(undefined);
+
+  constructor() {}
+
+  register(email: string, username: string, password: string): Observable<void> {
+    const promise = createUserWithEmailAndPassword(this.fireBaseAuth, email, password).then(
+      (response) => updateProfile(response.user, { displayName: username })
+    );
+
+    return from(promise);
+  }
+
+  login(email: string, password: string): Observable<void> {
+    const promise = signInWithEmailAndPassword(this.fireBaseAuth, email, password).then(() => {});
+
+    return from(promise);
+  }
+
+  logout(): Observable<void> {
+    const promise = signOut(this.fireBaseAuth);
+
+    return from(promise);
+  }
+
+  loginWithGoogle(): Observable<void> {
+    const promise = signInWithPopup(this.fireBaseAuth, new GoogleAuthProvider()).then(() => {});
+
+    return from(promise);
+  }
+
+  getUserIdToken(): Observable<string> {
+    const currentUser = this.fireBaseAuth.currentUser;
+    if (currentUser) {
+      const promise = currentUser.getIdToken();
+      return from(promise);
+    } else {
+      return from(Promise.resolve(''));
+    }
+  }
+}
