f51d9ecb7b7cb4a0ca4653036ff4f84c610b81e0
LinaAnule <152215917+LinaAnule@users.noreply.github.com>
2024-05-02 01:14:15 +0300

    upload agreement, global styles for background, buttions

--- vroom-frontend/src/app/components/main-page/main-page.component.ts
+++ vroom-frontend/src/app/components/main-page/main-page.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import {MatButton} from "@angular/material/button";
+
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton
+  ],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
