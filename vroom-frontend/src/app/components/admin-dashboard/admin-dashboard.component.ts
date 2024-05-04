19d5096341ca557cfbae953bd4e23a7281cc5376
Oskaras Safinas <143952279+Oskaras-coder@users.noreply.github.com>
2024-05-04 23:13:05 +0300

    added euribor term (#70)
    * added euribor term
    
    * changed duration in hours rounding

--- vroom-frontend/src/app/components/admin-dashboard/admin-dashboard.component.ts
+++ vroom-frontend/src/app/components/admin-dashboard/admin-dashboard.component.ts
@@ -307,6 +307,6 @@ export class AdminDashboardComponent {
 
   getDurationInHours(duration: string): number {
     const parsedDuration = this.parseDurationString(duration);
-    return Math.round(parsedDuration.days * 24 + parsedDuration.hours + parsedDuration.minutes / 60); ;
+    return parsedDuration.days * 24 + parsedDuration.hours + parsedDuration.minutes / 60; ;
   }
 }
