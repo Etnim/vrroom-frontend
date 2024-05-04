26f2c715602f9c08b0c26df1eea0261a07126f44
Xtrazyem <93625165+Xtrazyem@users.noreply.github.com>
2024-05-04 23:02:00 +0300

    Feature/application/submitted site (#69)
    * submission success page popup
    
    * Submission success popup card

--- vroom-frontend/src/app/components/wizard/submission-success/submission-success.component.ts
+++ vroom-frontend/src/app/components/wizard/submission-success/submission-success.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-submission-success',
   standalone: true,
-  imports: [],
+  imports: [ RouterModule, MatCardModule, MatIconModule, MatButtonModule],
   templateUrl: './submission-success.component.html',
   styleUrl: './submission-success.component.scss'
 })
 export class SubmissionSuccessComponent {
+  constructor(
+    private dialogRef: MatDialogRef<SubmissionSuccessComponent>,
+    private router: Router
+  ) {}
 
+  closeAndNavigate() {
+    this.dialogRef.close();
+    this.router.navigate(['/']);
+  }
 }
